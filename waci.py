611f2566982d93fcbc4ca245fe18624f16678d56
lpowersbg <lpower@sbgtv.com>
2021-05-10 14:57:44 -0400

    Changed to module setup

--- waci.py
+++ waci.py
@@ -1,41 +1,11 @@
 import flask
-from flask import Flask, jsonify, request
-import socket
-import binascii
+from flask import request
+import venus
+import monwall
 
 app = flask.Flask(__name__)
 # Enable for Debug
 app.config["DEBUG"] = False
-
-mhost = '192.168.0.1'
-mport = 7142
-
-vhost = '10.51.201.100'
-vport = 1000
-on = b'<T0000681001000079>'
-off = b'<T0000681000000078>'
-status = b'<T0000681100000079>'
-son = b'<F000068110100007a>'
-soff = b'<F0000681100000079>'
-goff = b'<F0000681000000078>'
-ven = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-def poweroff():
-    mon = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    mon.connect((mhost, mport))
-    message = b'\x01\x30\x2A\x30\x41\x30\x43\x02\x43\x32\x30\x33\x44\x36\x30\x30\x30\x34\x03\x1D\x0D'
-    mon.send(message)
-
-def poweron():
-    mon = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    mon.connect((mhost, mport))
-    message = b'\x01\x30\x2A\x30\x41\x30\x43\x02\x43\x32\x30\x33\x44\x36\x30\x30\x30\x31\x03\x18\x0D'
-    mon.send(message)
-
-def venus(ope):
-    ven.sendto(ope, (vhost, vport))
-    return ven.recvfrom(1024)[0]
-
 
 @app.route('/rpc/', methods=['POST'])
 def home():
@@ -48,26 +18,19 @@ def home():
     # print(request.form['Param4'])
     if request.form['Param1'] == 'MonOff':
         # print('poweroff')
-        poweroff()
+        monwall.monoff()
     elif request.form['Param1'] == 'MonOn':
         # print('poweron')
-        poweron()
+        monwall.monon()
     elif request.form['Param1'] == 'VenOn':
         # print('venuson')
-        if venus(status) == soff:
-            venus(on)
+        venus.venuson()
     elif request.form['Param1'] == 'VenRes':
         # print('venusres')
-        if venus(status) == son:
-            if venus(off) == goff:
-                reply = venus(status)
-                while reply != soff:
-                    reply = venus(status)
-                venus(on)
+        venus.venusres()
     elif request.form['Param1'] == 'VenOff':
         # print('venusoff')
-        if venus(status) == son:
-            venus(off)
+        venus.venusoff()
     return "success"
 
 # Enable for debug
